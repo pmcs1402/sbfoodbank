996a1a43c6f6546c04edb763bff50bd6a71b5913
Don Patterson <d_j_p_3@djp3.net>
2016-12-01 14:54:19 -0800

    Stubbing out s reverse geocode

--- ally/js/scripts.js
+++ ally/js/scripts.js
@@ -1,3 +1,15 @@
+//Stub for reverse_geocode
+function reverse_geocode(address){
+	//Right now we aren't actually doing the reverse geocoding
+	//Ideally we would be caching some of these so we don't overload the Google server 
+	var answer = {lat: 33.8121,
+				  lon: 117.9190+(Math.random()*10)};
+
+	return answer;
+}
+
+
+
 $ = jQuery.noConflict();
 $(function(){
 });
